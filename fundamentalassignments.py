0e63a7cd3e0a0d975e7fae46fb04e476a8adc75e
Dennis <to_dennis1234@yahoo.com>
2018-06-05 15:21:08 -0500

    update filter

--- fundamentalassignments.py
+++ fundamentalassignments.py
@@ -1,3 +1,5 @@
+Strings and Lists
+
 words = "It's thanksgiving day. It's my birthday,too!"
 print words.find("day")
 words.replace("day","month")
@@ -15,3 +17,43 @@ x.sort()
 x1=[x[0:len(x)/2]]
 x2=x[len(x)/2:len(x)]
 y=x1+x2
+
+
+Multiples,Sum,Average
+
+for x in range(0,1001):
+    print x
+
+for x in range(0,1000001):
+    if x%5==0:
+        print x
+
+a = [1, 2, 5, 10, 255, 3]
+y=0
+for x in a:
+    y+=x
+print y
+
+average= y/len(a)
+
+
+Filter by Type
+
+def filter(x):
+    if isinstance(x,int):
+       if x>=100:
+           print "That's a big number!"
+       else:
+           print "Thats a small number."
+    if isinstance(x,str):
+        if len(x)>=50:
+            print "Long sentence."
+        else:
+            print "Short sentence."
+    if isinstance(x,list):
+        if len(x)>=10:
+            print "Big list!"
+        else:
+            print "Short list."
+        
+
