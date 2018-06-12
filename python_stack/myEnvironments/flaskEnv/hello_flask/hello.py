8c29ac3267f63bcbd75b08466c18399a6209d309
Dennis <to_dennis1234@yahoo.com>
2018-06-12 15:26:51 -0500

    flask

---
+++ python_stack/myEnvironments/flaskEnv/hello_flask/hello.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', name="Dennis") 
+app.run(debug=True)
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
